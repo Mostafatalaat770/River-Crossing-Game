26ee4bd30ee9ce82c40f628ac7aed93a3ef56f88
Mostafa Talaat <mostafatalaat770@gmail.com>
2019-04-25 19:11:11 +0200

    Initial version
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

---
+++ src/Interfaces/IRiverCrossingController.java
@@ -0,0 +1,106 @@
+package Interfaces;
+
+import java.util.List;
+
+/**
+ * @author Mostafa Talaat
+ */
+public interface IRiverCrossingController {
+
+    /**
+     * this method initialize the controller with game strategy according
+     * to the level
+     *
+     * @param gameStrategy
+     */
+    void newGame(ICrossingStrategy gameStrategy);
+
+    /**
+     * resets the game without changing the strategy
+     */
+    void resetGame();
+
+    /**
+     * @return the current strategy instructions if the user want to see
+     * them
+     */
+    String[] getInstructions();
+
+    /**
+     * @return list of crossers on the right bank of the river
+     */
+    List<ICrosser> getCrossersOnRightBank();
+
+    /**
+     * @return list of crossers on the left bank of the river
+     */
+    List<ICrosser> getCrossersOnLeftBank();
+
+    /**
+     * @return determines whether the boat is on the left or on the right
+     * bank of the river
+     */
+    boolean isBoatOnTheLeftBank();
+
+    /**
+     * @return returns the number of sails that the user have done so far
+     */
+    int getNumberOfSails();
+
+    /**
+     * @param crossers            which the user selected to move
+     * @param fromLeftToRightBank boolean to inform the controller
+     *                            with the direction of the current game
+     * @return boolean if it is a valid move or not
+     */
+    boolean canMove(List<ICrosser> crossers, boolean
+            fromLeftToRightBank);
+
+    /**
+     * this method ysed to perform the move if it is valid
+     *
+     * @param crossers
+     * @param fromLeftToRightBank
+     */
+    void doMove(List<ICrosser> crossers, boolean
+            fromLeftToRightBank);
+
+    /**
+     * @return boolean providing that the undo action can be done or not
+     */
+    boolean canUndo();
+
+    /**
+     * @return boolean providing that the redo action can be done or not
+     */
+    boolean canRedo();
+
+    /**
+     * undo to the last game state
+     */
+    void undo();
+
+    /**
+     * redo the undo actions
+     */
+    void redo();
+
+    /**
+     * saves the game state
+     * (left bank crossers, right bank crossers, number of done sails,
+     * position of the boat)
+     */
+    void saveGame();
+
+    /**
+     * load the saved game state
+     */
+    void loadGame();
+
+    /**
+     * this function is bonus
+     * it returns the boat riders starting from the beginning of the game
+     * until the final solution to show the user the solution
+     */
+    List<List<ICrosser>> solveGame();
+}

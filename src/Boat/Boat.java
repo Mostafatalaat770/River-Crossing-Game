31427c17342cc3b28da0126dc6c21c3a7dd13c4d
Mostafa Talaat <mostafatalaat770@gmail.com>
2019-04-26 10:41:21 +0200

    Removed boat class and added the boat singleton
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/Boat/Boat.java
+++ src/Boat/Boat.java
@@ -1,11 +1,29 @@
 package Boat;
 
+/**
+ * @author Mostafa Talaat
+ */
 public class Boat {
+
     private int capacity;
     private boolean full;
+    private static Boat ourInstance = new Boat();
+
+    private Boat() {
+        capacity = 0;
+        full = false;
+    }
+
+    public static Boat getInstance() {
+        return ourInstance;
+    }
 
     public int getCapacity() {
         return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
     }
 
     public boolean isFull() {

aca3e11612e6d364268492c74be8877cb8317894
Mostafa Talaat <mostafatalaat770@gmail.com>
2019-05-02 00:25:00 +0200

    Added list of objects
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/Boat/Boat.java
+++ src/Boat/Boat.java
@@ -1,10 +1,16 @@
 package Boat;
 
+import Interfaces.ICrosser;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Mostafa Talaat
  */
 public class Boat {
 
+    List<ICrosser> raiders = new ArrayList<>();
     private int capacity;
     private boolean full;
     private static Boat ourInstance = new Boat();
@@ -16,6 +22,22 @@ public class Boat {
 
     public static Boat getInstance() {
         return ourInstance;
+    }
+
+    public static Boat getOurInstance() {
+        return ourInstance;
+    }
+
+    public static void setOurInstance(Boat ourInstance) {
+        Boat.ourInstance = ourInstance;
+    }
+
+    public List<ICrosser> getRaiders() {
+        return raiders;
+    }
+
+    public void setRaiders(List<ICrosser> raiders) {
+        this.raiders = raiders;
     }
 
     public int getCapacity() {
